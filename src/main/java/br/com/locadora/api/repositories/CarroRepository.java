793edd548fc13e7fb76d0e5c695b2f14803f169b
Rubem Chaves <108533543+vinicchaves@users.noreply.github.com>
2023-08-17 10:09:27 -0300

    atualização carro repository.

--- src/main/java/br/com/locadora/api/repositories/CarroRepository.java
+++ src/main/java/br/com/locadora/api/repositories/CarroRepository.java
@@ -1,9 +1,17 @@
 package br.com.locadora.api.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import br.com.locadora.api.domain.carro.*;
 
+import java.util.Date;
+import java.util.List;
+
 @Repository
 public interface CarroRepository extends JpaRepository<Carro, Long> {
+    Carro findCarroById(Long id);
+    @Query("SELECT DISTINCT a.carro FROM Aluguel a WHERE a.carro.id = :idCarro AND a.dataEntrega < :dataFinal AND a.dataDevolucao > :dataInicial AND a.carro.categoria = :categoria AND a.carro.descricao = :descricao AND a.carro.nome = :nome")
+    List<Carro> findCarrosAvailable(Long idCarro, Categoria categoria, String descricao, String nome, Date dataInicial, Date dataFinal);
 }
+
