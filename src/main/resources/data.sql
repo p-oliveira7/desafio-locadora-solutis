de43ef7181539283f39545d26120224c71997fc7
João Carlos Fernandes Bernardo <joaoc.bernardo12@gmail.com>
2023-08-21 23:31:47 -0300

    Modelos de Carros para adição na tabela

---
+++ src/main/resources/data.sql
@@ -0,0 +1,67 @@
+-- HATCH_COMPACTO
+INSERT INTO carros (tipo, placa, chassi, cor, valor_diaria, categoria, acessorio, descricao, nome, image_path)
+VALUES ('Hatch Compacto','ABC1234', '12345678901234567', 'Azul', 55.00, 'HATCH_COMPACTO', 'Ar condicionado',
+        'Hatch compacto com Ar condicionado', 'Suzuki Swift',
+        'https://images.pexels.com/photos/16840727/pexels-photo-16840727/free-photo-of-asfalto-rua-azul-carro.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1');
+
+-- HATCH_MEDIO
+INSERT INTO carros (tipo, placa, chassi, cor, valor_diaria, categoria, acessorio, descricao, nome, image_path)
+VALUES ('Hatch medio','DEF5678', '98765432109876543', 'Azul', 70.00, 'HATCH_MEDIO', 'Ar condicionado',
+        'Hatch médio com Ar condicionado', 'Mercedes-Benz',
+        'https://images.pexels.com/photos/810357/pexels-photo-810357.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1');
+
+-- SEDAN_COMPACTO
+INSERT INTO carros (tipo, placa, chassi, cor, valor_diaria, categoria, acessorio, descricao, nome, image_path)
+VALUES ('Sedan Compacto','GHI9012', '56789012345678901', 'Cinza', 50.00, 'SEDAN_COMPACTO', 'Câmbio automático',
+        'Sedan compacto com câmbio automático', 'Ford Focus',
+        'https://images.pexels.com/photos/1007410/pexels-photo-1007410.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1');
+
+INSERT INTO carros (tipo, placa, chassi, cor, valor_diaria, categoria, acessorio, descricao, nome, image_path)
+VALUES ('Sedan Compacto','JKL3456', '23456789012345678', 'Branco', 55.00, 'SEDAN_COMPACTO', 'Bancos de couro',
+        'Sedan compacto com bancos de couro', 'Chevrolet Cruze',
+        'https://images.pexels.com/photos/7030288/pexels-photo-7030288.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1');
+
+-- SEDAN_MEDIO
+INSERT INTO carros (tipo, placa, chassi, cor, valor_diaria, categoria, acessorio, descricao, nome, image_path)
+VALUES ('Sedan Medio','MNO6789', '34567890123456789', 'Preto', 60.00, 'SEDAN_MEDIO', 'Conexão Bluetooth',
+        'Sedan médio com sistema de áudio Bluetooth', 'Sedan Vintage',
+        'https://images.pexels.com/photos/1841120/pexels-photo-1841120.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1');
+
+INSERT INTO carros (tipo, placa, chassi, cor, valor_diaria, categoria, acessorio, descricao, nome, image_path)
+VALUES ('Sedan Medio','PQR1234', '45678901234567890', 'Preto', 65.00, 'SEDAN_MEDIO', 'Câmera de ré',
+        'Sedan com câmera de ré integrada', 'Volks Jetta',
+        'https://images.pexels.com/photos/17347155/pexels-photo-17347155/free-photo-of-tuned-volkswagen-jetta-on-road-in-forest.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1');
+
+-- SEDAN_GRANDE
+INSERT INTO carros (tipo, placa, chassi, cor, valor_diaria, categoria, acessorio, descricao, nome, image_path)
+VALUES ('Sedan Grande','STU5678', '56789012345678901', 'Branco', 70.00, 'SEDAN_GRANDE', 'Direção assistida',
+        'Sedan grande com direção assistida', 'Toyota Corolla',
+        'https://images.pexels.com/photos/11285174/pexels-photo-11285174.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1');
+
+INSERT INTO carros (tipo, placa, chassi, cor, valor_diaria, categoria, acessorio, descricao, nome, image_path)
+VALUES ('Sedan Grande','VWX9012', '67890123456789012', 'Azul', 80.00, 'SEDAN_GRANDE', 'Vidros elétricos',
+        'Sedan com vidros elétricos nas quatro portas', 'BMW i7',
+        'https://images.pexels.com/photos/170811/pexels-photo-170811.jpeg');
+
+-- UTILITARIO_COMERCIAL
+INSERT INTO carros (tipo, placa, chassi, cor, valor_diaria, categoria, acessorio, descricao, nome, image_path)
+VALUES ('Utilitário comercial','ABC5678', '98765432109876543', 'Preta', 120.00, 'UTILITARIO_COMERCIAL', 'GPS integrado',
+        'Utilitário com sistema de navegação GPS', 'Minivan Toyota',
+        'https://images.pexels.com/photos/18029633/pexels-photo-18029633/free-photo-of-cars-photos.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1');
+
+-- ESPORTIVO
+INSERT INTO carros (tipo, placa, chassi, cor, valor_diaria, categoria, acessorio, descricao, nome, image_path)
+VALUES ('Carro Esportivo','GHI3456', '23456789012345678', 'Azul', 150.00, 'ESPORTIVO', 'Teto solar panorâmico',
+        'Camaro com teto solar panorâmico', 'Chevrolet Camaro SS',
+        'https://images.pexels.com/photos/5800760/pexels-photo-5800760.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1');
+
+INSERT INTO carros (tipo, placa, chassi, cor, valor_diaria, categoria, acessorio, descricao, nome, image_path)
+VALUES ('Carro Esportivo','JKL6789', '34567890123456789', 'Cinza', 140.00, 'ESPORTIVO', 'Ar condicionado',
+        'Carro Esportivo com ar condicionado', 'Ford Mustang',
+        'https://images.pexels.com/photos/10905505/pexels-photo-10905505.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1');
+
+-- MINIVAN
+INSERT INTO carros (tipo, placa, chassi, cor, valor_diaria, categoria, acessorio, descricao, nome, image_path)
+VALUES ('Minivan utilitária','MNO1234', '45678901234567890', 'Azul', 90.00, 'MINIVAN', 'Direção assistida',
+        'Minivan com direção assistida', 'Wolks Kombi',
+        'https://images.pexels.com/photos/5085643/pexels-photo-5085643.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1');
